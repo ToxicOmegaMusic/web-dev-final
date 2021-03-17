3832fd97307c00ae714d2210b6dfd298ccbcfe08
Joshua Remlinger <pickichu@icloud.com>
2021-03-17 13:19:52 -0400

    .

--- flask_app.py
+++ flask_app.py
@@ -2,15 +2,15 @@ from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/store/")
+def store():
 	return render_template("store.html")
 
-@app.route("/cart", methods=["GET", "POST"])
+@app.route("/store/cart/", methods=["GET", "POST"])
 def cart():
 	if request.method == "POST":
 		result = request.form
 		return render_template("cart.html", result = result)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0', port=8000)
--- static/cart.js
+++ static/cart.js
@@ -1,7 +1,15 @@
 if (Object.keys(result)[0] != undefined) {
+	str = '';
 	for (item in result) {
-		console.log(stock[item].name);
+		str += `
+		<li>
+			<p>Name: ${stock[item].name}</p>
+			<p>Price: $${stock[item].price}</p>
+		</li>`;
 	}
+	logarea = document.querySelector('#logarea');
+	logarea.innerHTML = str;
+	console.log(str);
 } else {
-	console.log("Empty Cart!");
+	console.log('Empty Cart!');
 }
